455a99fdfa46470035f7c3427ee73986154e2131
Owen Lehmer <olehmer@gmail.com>
2017-09-13 16:18:45 -0700

    updated model

--- planetModel.py
+++ planetModel.py
@@ -1,6 +1,7 @@
 import numpy as np
-from math import pi, log, exp
-
+from math import pi, log, exp, sin, cos
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 
 class Panel:
@@ -9,11 +10,11 @@ class Panel:
     spherical coordinates. 
     """
 
-    def __init__(self, start_phi, end_phi, start_rho, end_rho):
+    def __init__(self, start_phi, end_phi, start_theta, end_theta):
         self.start_phi = start_phi
-        self.start_rho = start_rho
+        self.start_theta = start_theta
         self.end_phi = end_phi
-        self.end_rho = end_rho
+        self.end_theta = end_theta
 
     def set_albedo(self, albedo):
         """
@@ -22,28 +23,112 @@ class Panel:
         """
         self.albedo = [] #TODO: implement this later
 
-    def get_cartesian_coordinates(planet_radius):
+    def get_cartesian_coordinates(self,planet_radius):
         """
         Return the x,y,z coordinates of the panel.
         """
-        x,y,z = 0 #TODO: implement
-        return [(x,y,z),(x,y,z),(x,y,z),(x,y,z)]
+
+        x0 = planet_radius*sin(self.start_phi)*cos(self.start_theta)
+        y0 = planet_radius*sin(self.start_phi)*sin(self.start_theta)
+        z0 = planet_radius*cos(self.start_phi)
+
+        x1 = planet_radius*sin(self.start_phi)*cos(self.end_theta)
+        y1 = planet_radius*sin(self.start_phi)*sin(self.end_theta)
+        z1 = planet_radius*cos(self.start_phi)
+
+        x2 = planet_radius*sin(self.end_phi)*cos(self.start_theta)
+        y2 = planet_radius*sin(self.end_phi)*sin(self.start_theta)
+        z2 = planet_radius*cos(self.end_phi)
+
+        x3 = planet_radius*sin(self.end_phi)*cos(self.end_theta)
+        y3 = planet_radius*sin(self.end_phi)*sin(self.end_theta)
+        z3 = planet_radius*cos(self.end_phi)
+
+        return [x0,x1,x2,x3], [y0,y1,y2,y3], [z0,z1,z2,z3]
 
 class Planet:
     """
     Create a planet made out of flat panels. 
     """
 
-    def __init__(self, radius, num_panels):
+    def __init__(self, radius, resolution):
         """
         Initialize the planet.
 
         Inputs:
         radius - the planet radius [m]
-        num_panels - the number of flat panels in each hemisphere of the planet.
+        resolution - the number of flat panels for the planet.
         """
 
         self.radius = radius
-        self.num_panels = num_panels
+        self.resolution = resolution 
 
-    def generate_panels():
+        self.generate_panels()
+
+
+    def generate_panels(self):
+        """
+        Generate the panels of the planet
+        """
+
+        self.panels = []
+
+        phi_steps = np.linspace(0, pi, self.resolution)
+        theta_steps = np.linspace(0, 2.0*pi, self.resolution)
+
+        for i in range(0,len(phi_steps)-1):
+            for j in range(0,len(theta_steps)-1):
+                self.panels.append( Panel(phi_steps[i], phi_steps[i+1], \
+                        theta_steps[j], theta_steps[j+1]) )
+
+    def plot_planet(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        count = 0
+        for panel in self.panels:
+            x,y,z = panel.get_cartesian_coordinates(self.radius)
+            print("\n%d:"%(count))
+            count += 1
+            print(x)
+            print(y)
+            print(z)
+            ax.plot_surface(x,y,z, linewidth=2)
+            break
+
+        plt.show()
+
+def test():
+    p = Planet(10.0, 10)
+    p.plot_planet()
+
+test()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
